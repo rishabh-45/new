953f7c34325cc364dbcdf480970878caf4b98c71
Raj dubey <com.mailuser@gmail.com>
2020-04-19 18:02:21 +0530

    remove installer of driver

--- stdplugins/install_chromedriver.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-wget  https://chromedriver.storage.googleapis.com/80.0.3987.106/chromedriver_linux64.zip
-unzip -o chromedriver_linux64.zip -d /app/.chromedriver/bin/
-unzip -o chromedriver_linux64.zip 
-
-rm chrome*.zip*
