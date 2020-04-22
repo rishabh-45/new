7c9fd7c2c736eeaa46b1f08c7fed09b6dd463e71
rishabh-45 <62442678+rishabh-45@users.noreply.github.com>
2020-04-22 10:26:14 +0530

    Update updater.py

--- stdplugins/updater.py
+++ stdplugins/updater.py
@@ -21,7 +21,7 @@ IS_SELECTED_DIFFERENT_BRANCH = (
     "in this case, Updater is unable to identify the branch to be updated."
     "please check out to an official branch, and re-start the updater."
 )
-OFFICIAL_UPSTREAM_REPO = "https://github.com/authoritydmc/BEASTBOT-REBORN"
+OFFICIAL_UPSTREAM_REPO = "https://github.com/rishabh-45/new"
 BOT_IS_UP_TO_DATE = "`The BeastBot up-to-date.\nThank you for Using this Service.`"
 NEW_BOT_UP_DATE_FOUND = (
     "new update found for {branch_name}\n"
