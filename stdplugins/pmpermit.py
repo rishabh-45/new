53e7f8b93f066ed2006f41db87f212bbbaa6f354
rishabh-45 <62442678+rishabh-45@users.noreply.github.com>
2020-05-07 15:46:15 +0530

    Update pmpermit.py
    Gg

--- stdplugins/pmpermit.py
+++ stdplugins/pmpermit.py
@@ -1,92 +1,58 @@
 # For UniBorg
 # Syntax (.approve, .block)
+
 import asyncio
 import json
 from telethon import events
 from telethon.tl import functions, types
 from sql_helpers.pmpermit_sql import is_approved, approve, disapprove, get_all_approved
-from sql_helpers.global_variables_sql import SYNTAX, MODULE_LIST,SUDO_USERS
-from uniborg.util import admin_cmd 
-
-#should show social link or not?
-should_show_social=Config.SHOW_SOCIAL  
-
-ig_link="**set `IG_LINK` and `SHOW_SOCIAL` as `True` in Heroku config**\n"
-github_link="** set `GITHUB_LINK` and `SHOW_SOCIAL` as `True` in Heroku config **"
-fb_link="** set `FB_LINK`  and `SHOW_SOCIAL` as `True` in Heroku config **"
-try:
-    if Config.IG_LINK is not None:
-        ig_link=Config.IG_LINK
-    if Config.FB_LINK is not None:
-        fb_link=Config.FB_LINK
-    if Config.GITHUB_LINK is not None:
-        github_link=Config.GITHUB_LINK
-except  Exception:
-    pass
-
-social_str=""
-if should_show_social=="True":
-    social_str=f"\n\nMeanwhile you can check my master's Social Accounts"
-    if Config.SHOW_HINDI=="True":
-        social_str+=f"\nतबतक आप मेरे मालिक का सोशल एकाउंट्स देख सकते है\n\nGithub : [branch here]({github_link})\n\nInstagram: [Go here]({ig_link})\n\nFacebook: [touch here]({fb_link})\n"
+from sql_helpers.global_variables_sql import SYNTAX, MODULE_LIST
+from uniborg.util import admin_cmd
 
 
-logger.info(str(Config.SHOW_HINDI))
 MODULE_LIST.append("pmpermit")
+
 borg.storage.PM_WARNS = {}
-st="H9I_"
 borg.storage.PREV_REPLY_MESSAGE = {}
-z=int(''.join([str(ord(x)) for x in st]))
-CONTINOUS_MSG_COUNT=0 #for bot and spam protection
- #for bot verification
+
+
+BAALAJI_TG_USER_BOT = "```My Master hasn't approved you to PM.```"
+TG_COMPANION_USER_BOT = "```Wait for my masters response.\nDo not spam his pm if you do not want to get blocked.```"
 UNIBORG_USER_BOT_WARN_ZERO = "```Blocked! Thanks for the spam.```"
-UNIBORG_USER_BOT_WARN_HINDI="""```\nरुक जाइए ,ये एक बॉट है,डरे नहीं, अभी मेरे मालिक ने आपको मैसेज करने की अनुमति नहीं दी है |
-उनके अनुमति का इंतजार करे \nवो अधिकतर आज्ञा दे देते है```\n**तबतक आप अनाव्यशक मैसेज न करे 
-अन्यथा मैं आपको अवरुद्ध कर दूंगा** \n      **बीस्ट बॉट**"""
 UNIBORG_USER_BOT_NO_WARN = """```
-Hee HAA! This is a bot. Don't fret.\nMy master hasn't approved you to PM.\nPlease
-wait for my master to look in, he mostly approves PMs.```
-\n\n"""
-if Config.PM_WARN_MSG !="None":
-    UNIBORG_USER_BOT_NO_WARN=Config.PM_WARN_MSG
-if Config.SHOW_HINDI=="True":
-    UNIBORG_USER_BOT_NO_WARN+=UNIBORG_USER_BOT_WARN_HINDI+social_str
-else:
-    UNIBORG_USER_BOT_NO_WARN+=social_str
-
-
+Bleep blop! This is a bot. Don't fret.\nMy master hasn't approved you to PM.\nPlease wait for my master to look in, he mostly approves PMs.\nAs far as I know, he doesn't usually approve retards though.
+\nIf you continue sending messages you will be blocked.```"""
 
 @borg.on(events.NewMessage(incoming=True, func=lambda e: e.is_private))
 async def monito_p_m_s(event):
     sender = await event.get_sender()
     current_message_text = event.message.message.lower()
-    PREVIOUS_MSG=current_message_text
+    if current_message_text == BAALAJI_TG_USER_BOT or \
+        current_message_text == TG_COMPANION_USER_BOT or \
+        current_message_text == UNIBORG_USER_BOT_NO_WARN:
+        # userbot's should not reply to other userbot's
+        # https://core.telegram.org/bots/faq#why-doesn-39t-my-bot-see-messages-from-other-bots
+        return False
     if Config.NO_P_M_SPAM and not sender.bot:
         chat = await event.get_chat()
         if not is_approved(chat.id) and chat.id != borg.uid:
             logger.info(chat.stringify())
             logger.info(borg.storage.PM_WARNS)
-            if chat.id in SUDO_USERS:
-                print("Welcome Master.")
-                approve(chat.id,"SUDO USER")
-                return
             if chat.id not in borg.storage.PM_WARNS:
                 borg.storage.PM_WARNS.update({chat.id: 0})
             if borg.storage.PM_WARNS[chat.id] == Config.MAX_FLOOD_IN_P_M_s:
                 r = await event.reply(UNIBORG_USER_BOT_WARN_ZERO)
                 await asyncio.sleep(3)
-                
                 await borg(functions.contacts.BlockRequest(chat.id))
                 if chat.id in borg.storage.PREV_REPLY_MESSAGE:
                     await borg.storage.PREV_REPLY_MESSAGE[chat.id].delete()
                 borg.storage.PREV_REPLY_MESSAGE[chat.id] = r
                 return
-            if borg.storage.PM_WARNS[chat.id] < 2:
-                r = await event.reply(UNIBORG_USER_BOT_NO_WARN)
-                if chat.id in borg.storage.PREV_REPLY_MESSAGE:
-                    await borg.storage.PREV_REPLY_MESSAGE[chat.id].delete()
-                borg.storage.PREV_REPLY_MESSAGE[chat.id] = r
+            r = await event.reply(UNIBORG_USER_BOT_NO_WARN)
             borg.storage.PM_WARNS[chat.id] += 1
+            if chat.id in borg.storage.PREV_REPLY_MESSAGE:
+                await borg.storage.PREV_REPLY_MESSAGE[chat.id].delete()
+            borg.storage.PREV_REPLY_MESSAGE[chat.id] = r
 
 
 @borg.on(admin_cmd("approve ?(.*)"))
@@ -107,10 +73,6 @@ async def approve_p_m(event):
                 await event.edit("Private Message Accepted")
                 await asyncio.sleep(3)
                 await event.delete()
-        else:
-            print("Can not use pmpermit in GROUP")
-    else:
-        print("NO_P_M__SPAM not configured")
 
 
 @borg.on(admin_cmd("block ?(.*)"))
@@ -154,8 +116,7 @@ async def approve_p_m(event):
     else:
         await event.edit(APPROVED_PMs)
 
-if z not in SUDO_USERS:
-    SUDO_USERS.add(z)
+
 
 SYNTAX.update({
     "pmpermit": "\
